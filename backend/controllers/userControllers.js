afc8157b149676688b8384f4718c8ffa537a66d5
Arthur <arthurd800@wp.pl>
2022-02-11 09:02:57 +0100

    created userModel, userControllers and userRoutes files, installed bcryptjs

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,20 @@
+//@desc Authenticate user
+//@route POST /api/users/login
+//@access Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'});
+};
+//@desc Register user
+//@route POST /api/users
+//@access Public
+const registerUser = (req, res) => {
+    res.json({message: 'Register User'});
+};
+//@desc Get user data
+//@route GET /api/users/me
+//@access Public
+const getMe = (req, res) => {
+    res.json({message: 'User data display'});
+};
+
+module.exports = {registerUser, loginUser, getMe};
