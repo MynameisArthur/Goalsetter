deb98eaddbc02bffda54998d084f97937dd6d356
Arthur <arthurd800@wp.pl>
2022-02-18 09:54:48 +0100

    finished developing register and login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react';
 import {FaSignInAlt} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {login, reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -7,6 +12,20 @@ function Login() {
         password: '',
     });
     const {email, password} = formData;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    );
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess || user) {
+            navigate('/');
+        }
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -15,7 +34,15 @@ function Login() {
     };
     const onSubmit = (e) => {
         e.preventDefault();
+        const userData = {
+            email,
+            password,
+        };
+        dispatch(login(userData));
     };
+    if (isLoading) {
+        return <Spinner />;
+    }
     return (
         <>
             <section className='heading'>
@@ -39,7 +66,7 @@ function Login() {
                     </div>
                     <div className='form-group'>
                         <input
-                            type='passwod'
+                            type='password'
                             className='form-control'
                             id='password'
                             name='password'
@@ -49,7 +76,7 @@ function Login() {
                         />
                     </div>
                     <div className='form-group'>
-                        <button type='submit' class='btn btn-block'>
+                        <button type='submit' className='btn btn-block'>
                             Submit
                         </button>
                     </div>
